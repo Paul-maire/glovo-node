c63ef6a477e9ef5eed0d76148248f5203f189fc0
Paul-maire <paul.maire@hetic.net>
2020-04-18 17:19:22 +0200

    fix orders.list

--- libs/orders.js
+++ libs/orders.js
@@ -1,19 +1,27 @@
 module.exports = client => {
 
-	const deliveryArea = async _ => await client.get(`/b2b/working-areas`)
+	const deliveryArea = async _ => await client.get('/b2b/working-areas')
 
-	const estimate = async body => await client.post(`/b2b/orders/estimate`, body)
+	const estimate = async body => await client.post('/b2b/orders/estimate', body)
 
-	const order = async body => await client.post(`/b2b/orders`, body)
+	const order = async body => await client.post('/b2b/orders', body)
 
 	const cancel = async order_id => await client.post(`/b2b/orders/${order_id}/cancel`)
 
 	const find = async order_id => await client.get(`/b2b/orders/${order_id}`)
 
-	const list = async ({ limit = null , offset = null }) => {
-		console.error('TODO: stringify limit and offset for query')
-		return
-		// return await client.get(`/b2b/orders`)
+	const list = async ({ limit = null , offset = null } = {}) => {
+
+		let url = '/b2b/orders'
+
+		const params = {}
+		if (limit) params.limit = limit
+		if (offset) params.offset = offset
+
+		const search = new URLSearchParams(params).toString()
+		if (search) url = `${url}?${search.toString()}`
+
+		return await client.get(url)
 	}
 
 	return {
